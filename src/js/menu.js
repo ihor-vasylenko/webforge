43bef2c5badadccc1b58e648ea96401cf29bd6ac
MarynaZ <maryzubritskaya@gmail.com>
2025-05-22 22:26:16 +0300

    edits footer

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,10 +3,18 @@
       openModalBtn: document.querySelector("[data-menu-open]"),
       closeModalBtn: document.querySelector("[data-menu-close]"),
       modal: document.querySelector("[data-menu]"),
+      menuLinks: document.querySelectorAll(".mobile-nav-link"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
+
+     refs.menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      refs.modal.classList.remove("is-open");
+      document.body.classList.remove("no-scroll");
+    });
+  });
   
     function toggleModal() {
       refs.modal.classList.toggle("is-open");
